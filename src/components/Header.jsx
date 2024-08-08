75c3c132bdbb8cd8459d71d44c4e9ddc5e9a538a
tatsuya98 <nathanbailey_62@hotmail.com>
2024-08-08 10:36:15 +0100

    completed task10

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { fetchTopics } from "../../newsApi";
 const Header = () => {
+  const [topics, setTopics] = useState([]);
+  useEffect(() => {
+    fetchTopics().then((topics) => {
+      setTopics(topics);
+    });
+  }, []);
   return (
     <>
-      <header>
+      <header className="flex-header">
         <Link className="home" to="/">
           <h1>nc news</h1>
         </Link>
+        <nav>
+          <ul className="flex-nav">
+            {topics.map((topic) => {
+              return (
+                <Link key={topic.slug} to={`/${topic.slug}`}>
+                  <li>{topic.slug}</li>
+                </Link>
+              );
+            })}
+          </ul>
+        </nav>
       </header>
     </>
   );
